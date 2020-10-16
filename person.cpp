f5b914698bdb5484f8ab7f1033ef4ac760bc31f8
jsegu447 <jsegu010@ucr.edu>
2020-10-15 18:38:26 -0700

    fixed size error and fixed leak in compute relation

--- person.cpp
+++ person.cpp
@@ -70,9 +70,8 @@ char* Person::compute_relation(int level){
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
-        temp = temp2;
-        temp2 = nullptr;
-        delete[] temp2;
+       delete[] temp;
+         temp = temp2;
     }
     return temp;
 }
